86586bff6aa9455c300c5c2fb38a46782e7ccd10
Nayeem Khan <knayeem401416@gmail.com>
2024-04-27 23:52:50 +0600

    Chat Application

---
+++ lib/services/user_management_services.dart
@@ -0,0 +1,25 @@
+import 'package:chat_application/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserManage {
+  final firebase = FirebaseFirestore.instance;
+
+  Future<void> createUserProfile(
+      {required String userName,
+      required String userEmail,
+      required String userID}) async {
+    var user = UserData(
+      name: userName,
+      uuid: userID,
+      email: userEmail,
+    );
+
+    await firebase.collection("users").doc(userID).set(user.toMap());
+
+    // await firebase.collection("users").add({
+    //   "name": userName,
+    //   "email": userEmail,
+    //   "my_chats": FieldValue.arrayUnion([]),
+    // });
+  }
+}
