bccc66c4b1f67cde30dfb7704079dafa15a5065d
Nayeem Khan <knayeem401416@gmail.com>
2024-04-30 09:50:20 +0600

    Chat Application

--- lib/screens/chat/chat_tabs/chats.dart
+++ lib/screens/chat/chat_tabs/chats.dart
@@ -1,10 +1,12 @@
-import 'package:chat_application/const_config/text_config.dart';
-import 'package:chat_application/widgets/custom_buttons/Rouded_Action_Button.dart';
 import 'package:chat_application/widgets/input_widgets/simple_input_field.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:random_avatar/random_avatar.dart';
 
 import '../../../const_config/color_config.dart';
+import '../../../models/user_model.dart';
 import '../../../services/chat_service.dart';
 
 class ChatsPage extends StatefulWidget {
@@ -17,64 +19,157 @@ class ChatsPage extends StatefulWidget {
 class _ChatsPageState extends State<ChatsPage> {
   final firebase = FirebaseFirestore.instance;
   final messageController = TextEditingController();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<String> _fetchUserName(String uuid) async {
+    var doc = await firebase.collection('users').doc(uuid).get();
+    if (doc.exists) {
+      UserData user = UserData.fromMap(doc.data()!);
+      return user.name ?? 'Unknown User';
+    }
+    return 'Unknown User';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: MyColor.scaffoldColor,
-        body: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder(
-                stream: firebase.collection('chat').orderBy('time').snapshots(),
-                builder: (context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData && snapshot.connectionState == ConnectionState.active) {
-                    var data = snapshot.data.docs;
-                    return data.length != 0
-                        ? ListView.builder(
-                            itemCount: data.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.all(2.0),
-                                child: Card(
-                                  color: Colors.white,
-                                  child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(snapshot.data.docs[index]['message']),
-                                ),),
-                              );
-                            },
-                          )
-                        : const Center(child: Text("No Chats to show"));
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
-            ),
-            SimpleInputField(
-              controller: messageController,
-              hintText: "Aa..",
-              needValidation: true,
-              errorMessage: "Message box can't be empty",
-              fieldTitle: "",
-              needTitle: false,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            RoundedActionButton(
-              onClick: () {
-                ChatService().sendChatMessage(message: messageController.text);
+      backgroundColor: MyColor.scaffoldColor,
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: firebase.collection('chat').orderBy('time').snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.active) {
+                  var data = snapshot.data!.docs;
+
+                  return data.length != 0
+                      ? ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            var message =
+                                data[index].data() as Map<String, dynamic>;
+                            var uuid = message['uuid'] as String?;
+                            bool isCurrentUser = uuid == auth.currentUser?.uid;
+
+                            return Align(
+                              alignment: isCurrentUser
+                                  ? Alignment.centerRight
+                                  : Alignment.centerLeft,
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 4.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    if (!isCurrentUser)
+                                      FutureBuilder<String>(
+                                          future: _fetchUserName(uuid!),
+                                          builder: (context, snapshot) {
+                                            if (snapshot.connectionState ==
+                                                ConnectionState.done) {
+                                              return RandomAvatar(
+                                                snapshot.data!,
+                                                trBackground: false,
+                                                height: 20,
+                                              );
+                                            } else {
+                                              return const SizedBox(
+                                                height: 20,
+                                                child:
+                                                    CircularProgressIndicator(),
+                                              );
+                                            }
+                                          }),
+                                    const SizedBox(width: 10),
+                                    Expanded(
+                                      child: Card(
+                                        color: isCurrentUser
+                                            ? MyColor.primary
+                                            : MyColor.white,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            message['message'] as String? ??
+                                                'No message content',
+                                            style: TextStyle(
+                                              color: isCurrentUser
+                                                  ? MyColor.white
+                                                  : MyColor.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    if (isCurrentUser)
+                                      FutureBuilder<String>(
+                                        future: _fetchUserName(
+                                            auth.currentUser!.uid),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.done) {
+                                            return RandomAvatar(
+                                              snapshot.data!,
+                                              trBackground: false,
+                                              height: 35,
+                                            );
+                                          } else {
+                                            return const SizedBox(
+                                              height: 35,
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            );
+                                          }
+                                        },
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      : const Center(child: Text("No Chats to show"));
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
               },
-              label: "Send Message",
             ),
-            const SizedBox(
-              height: 10,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: SimpleInputField(
+                    controller: messageController,
+                    hintText: "Type a message...",
+                    needValidation: true,
+                    errorMessage: "Message box can't be empty",
+                    fieldTitle: "",
+                    needTitle: false,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(LineAwesomeIcons.paper_plane,
+                      color: MyColor.primary),
+                  onPressed: () {
+                    if (messageController.text.isNotEmpty) {
+                      ChatService().sendChatMessage(
+                        message: messageController.text,
+                      );
+                      messageController.clear();
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/chat/chat_tabs/discover.dart
+++ lib/screens/chat/chat_tabs/discover.dart
@@ -1,6 +1,10 @@
 import 'package:chat_application/const_config/color_config.dart';
 import 'package:chat_application/const_config/text_config.dart';
+import 'package:chat_application/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:random_avatar/random_avatar.dart';
+import 'package:intl/intl.dart';
 
 class DiscoverPage extends StatefulWidget {
   const DiscoverPage({super.key});
@@ -10,15 +14,123 @@ class DiscoverPage extends StatefulWidget {
 }
 
 class _DiscoverPageState extends State<DiscoverPage> {
+  final FirebaseFirestore firebase = FirebaseFirestore.instance;
+
+  Stream<List<UserData>> getUsersStream() {
+    return firebase.collection('users').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => UserData.fromMap(doc.data())).toList());
+  }
+
+  Future<DateTime?> getLastActive(String uuid) async {
+    var lastMessageSnapshot = await firebase
+        .collection('chat')
+        .where('uuid', isEqualTo: uuid)
+        .orderBy('time', descending: true)
+        .limit(1)
+        .get();
+
+    if (lastMessageSnapshot.docs.isNotEmpty) {
+      Timestamp lastActive = lastMessageSnapshot.docs.first.get('time');
+      return lastActive.toDate();
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColor.scaffoldColor,
-      body: Center(
-        child: Text(
-          "Updates Coming soon....",
-          style: TextDesign().bodyTextSmall,
-        ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "Registered Users",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: MyColor.primary,
+              ),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<List<UserData>>(
+              stream: getUsersStream(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text("Error: ${snapshot.error}");
+                } else if (snapshot.hasData) {
+                  var users = snapshot.data!;
+                  if (users.isEmpty) {
+                    return Text("No users found",
+                        style: TextDesign().bodyTextSmall);
+                  }
+                  return ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      UserData user = users[index];
+                      return FutureBuilder<DateTime?>(
+                        future: getLastActive(user.uuid!),
+                        builder: (context, lastActiveSnapshot) {
+                          String lastActiveText = "Never Active";
+                          if (lastActiveSnapshot.hasData &&
+                              lastActiveSnapshot.data != null) {
+                            lastActiveText = DateFormat('dd/MM/yyyy hh:mm a')
+                                .format(lastActiveSnapshot.data!);
+                          }
+                          return Container(
+                            margin: const EdgeInsets.all(8.0),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 15.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  spreadRadius: 1,
+                                  blurRadius: 3,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    RandomAvatar(
+                                      user.name ?? 'Unknown User',
+                                      trBackground: false,
+                                      height: 40,
+                                      width: 40,
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(user.name ?? 'Unknown User',
+                                        style: TextDesign().bodyTextSmall),
+                                  ],
+                                ),
+                                Text(lastActiveText,
+                                    style: TextStyle(color: Colors.grey[600]))
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                } else {
+                  return Text("No data available",
+                      style: TextDesign().bodyTextSmall);
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
